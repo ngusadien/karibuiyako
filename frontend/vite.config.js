6129588b9b566791ad47f18e62b386931c8075df
ngusadien <ngusa41@gmail.com>
2025-04-22 06:45:38 +0100

    to allow Render preview host in Vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
-  ],
+  plugins: [react()],
   preview: {
+    host: '0.0.0.0', // REQUIRED for Render
     port: process.env.PORT || 4173,
-    host: true,
+    strictPort: true,
     allowedHosts: ['karibuyako-front.onrender.com']
   }
 })
