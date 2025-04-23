9e64a946e2b66ab4fa624bd824bffdd274624b60
ngusadien <ngusa41@gmail.com>
2025-04-23 18:48:12 +0100

    style issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './',
   preview: {
     host: '0.0.0.0', // REQUIRED for Render
     port: process.env.PORT || 4173,
